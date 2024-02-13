697604d267e749b58c43b4bb18a44baf9fa45bb0
crypt0nX <spencer_clay@foxmail.com>
2024-02-13 20:54:22 +0800

    add check_arguments

--- python script/positionHelper.py
+++ python script/positionHelper.py
@@ -6,8 +6,12 @@
 # @annotation    :
 import argparse
 import ast
+import logging
+import sys
 
 unit = "usdt"
+
+
 # transactions = [(0.1974, 200), (0.1944, 800)]
 
 
@@ -28,6 +32,22 @@ def calculate_profit_loss(_size, entry, _tp, _sl):
     return round(_profit, 3), round(_loss, 3), round(_profit / _loss, 3)
 
 
+def check_arguments(_tp, _sl, _transactions):
+    if _tp > _sl:
+        for _transaction in _transactions:
+            if not (_sl < _transaction[0] < _tp):
+                logging.error("entry price is wrong!!")
+                sys.exit()
+    elif _tp < _sl:
+        for _transaction in _transactions:
+            if not (_tp < _transaction[0] < _sl):
+                logging.error("entry price is wrong!!")
+                sys.exit()
+    else:
+        logging.error("tp or sl is wrong!!")
+        sys.exit()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate average price from transactions")
     # parser.add_argument("--size", type=float, help="Specify size.")
@@ -41,11 +61,14 @@ if __name__ == "__main__":
     tp = args.tp
     sl = args.sl
     transactions = ast.literal_eval(args.transactions)
+    check_arguments(tp, sl, transactions)
+    side = "long📈" if tp > sl else "short📉"
     size, entry_price = calculate_average_price(transactions)
 
     profit, loss, RR = calculate_profit_loss(size, entry_price, tp, sl)
 
     print(f"\033[33maverage price: {str(round(entry_price, 5))} {unit}\033[0m\n"
+          f"side: {str(side)}\n"
           f"size: {str(size)} {unit}\n"
           f"\033[32mprofit🤑: {str(profit)} {unit}\033[0m\n"
           f"\033[31mloss🤮: {str(loss)} {unit}\033[0m\n"
